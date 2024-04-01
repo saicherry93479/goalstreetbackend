8a0e977e0cca672221c96674e8c73eace2364f0d
cherry <70464077+saicherry93479@users.noreply.github.com>
2024-04-01 21:23:07 +0530

    Update Work.js

--- models/Work.js
+++ models/Work.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose');
 const WorkDataSchema = new mongoose.Schema({
     jobName: String,
     companyName: String,
-    postedDate: {type:Date,default:Date.now},
+    postedDate: { type: Date, default: Date.now },
     jobDescription: String,
     domain: String,
     salaryRange: String,
     officeType: { type: String }, // Enum for office type
-    jobType: String
+    jobType: String,
+    addedBy: {
+        type:String,
+        default:""
+    }
 });
 
 module.exports = mongoose.model('WorkData', WorkDataSchema);

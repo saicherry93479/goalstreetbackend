e5d425c778f352f9c1f5b42260bfb170dc045f3d
cherry <70464077+saicherry93479@users.noreply.github.com>
2024-04-01 19:34:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,47 +3,64 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const formController = require("./controllers/formController");
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 var cors = require("cors");
-const { addWorkData, renderAddWorkDataForm, getWorkTable, editWork, updateEditWork, sendWorkDataToFrontEnd,deleteWork } = require("./controllers/workController");
+const { addWorkData, renderAddWorkDataForm, getWorkTable, editWork, updateEditWork, sendWorkDataToFrontEnd, deleteWork } = require("./controllers/workController");
 const { login, authenticate, logout } = require("./controllers/authController");
+const { submitHrForm, getHrFormData } = require("./controllers/hrFormController");
+const { sendAccessEmail } = require("./controllers/emailController");
+const { notFoundPage } = require("./controllers/utilController");
 
 const secretKey = 'mysecretkey_goalstreet';
 
-function authenticationMiddleware(req, res, next) {
-  console.log('request path ', req.path)
-  if (req.path === '/' && req.method === 'POST') {
-    console.log('request came ', req.method)
+async function authenticationMiddleware(req, res, next) {
+  let authtoken = req.query.authtoken;
+  console.log("in authenticate middleware ")
+  console.log("request is ", req.path, " - method is ", req.method)
+  console.log("params is ", req.query)
+  console.log("authtoken is ", authtoken)
+  if (req.path === '/') {
     next();
-  }else if(req.path==='/frontendWorkData' || req.path==='/submit'){
-    next();
-  } else {
-    const authtoken = req.query.authtoken;
-    console.log(req.query);
-    console.log("params ", authtoken);
+    return;
+  }
 
-    if (authtoken !== null && authtoken !== undefined) {
-      console.log("type of authtoken ", typeof (authtoken))
-      try {
-        const verified = jwt.verify(authtoken, secretKey);
-        // If token is verified, proceed to the next middleware or controller
-        if (req.path === '/' && req.method === 'GET') {
-          res.redirect("/dashboard")
-          return;
-        }
-        next();
-      } catch (error) {
-        // If token is not verified, render login page
-        console.log("verification jwt   failed", error);
-        res.render("LoginPage.ejs", { loginData: {}, errors: {} });
-      }
-    } else {
-      // If authtoken is not present, render login page
-      res.render("LoginPage.ejs", { loginData: {}, errors: {} });
+  if (authtoken === null || authtoken === undefined) {
+    console.log("req.cokkies is ",req.cookies)
+    if (req.cookies && req.cookies.auth) {
+        authtoken = req.cookies.auth;
+        console.log("autoken presnets ",authtoken)
+    }
+    else {
+      res.redirect("/")
+      return;
     }
   }
-}
+  try {
 
+    const verified = jwt.verify(authtoken, secretKey);
+    req.user = verified;
+    console.log("verified is ", verified)
+    if (verified.role === 'HR') {
+      if (req.path === '/workdata/add' || req.path === '/worktable' || req.path === '/works/edit/:id' || req.path === '/works/delete/:id' || req.path === '/pageNotFound') {
+        next();
+        return;
+      } else {
+        res.redirect(`/pageNotFound?authtoken=${authtoken}`)
+        return
+      }
+    } else if (verified.role === 'admin') {
+      console.log("it is admin")
+      next();
+      return;
+    }
+
+  } catch (e) {
+    console.log("error is ", e)
+    res.redirect("/")
+  }
+
+}
 
 
 const app = express();
@@ -53,9 +70,11 @@ app.use(cors());
 // Parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(authenticationMiddleware)
 
 const url='mongodb+srv://cherryworkspacemail:cherryworkspacemail@cluster0.qqvoel7.mongodb.net/?retryWrites=true&w=majority';
+
 
 
 // Connect to MongoDB
@@ -92,7 +111,11 @@ app.get("/frontendWorkData", sendWorkDataToFrontEnd)
 app.get("/", login)
 app.post('/', authenticate)
 app.get('/logout', logout)
-app.get("/works/delete/:id",deleteWork)
+app.get("/works/delete/:id", deleteWork)
+app.post("/submitHrForm", submitHrForm)
+app.get("/hrFormPage", getHrFormData)
+app.get("/sendAccessEmail", sendAccessEmail);
+app.get("/pageNotFound", notFoundPage)
 
 // const url = "mongodb://localhost/mydatabase";
 // mongoose.connect(url);
@@ -101,6 +124,7 @@ const start = async () => {
   try {
     await mongoose
       .connect(url)
+    console.log("connected");
     app.listen(port, () => {
       console.log("server is running on port", port);
     });

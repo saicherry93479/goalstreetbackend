7687f413689266c9e7675e0f6a8f7b4e73db1582
cherry <70464077+saicherry93479@users.noreply.github.com>
2024-04-05 19:45:32 +0530

    Update emailController.js

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -6,15 +6,15 @@ exports.sendAccessEmail = async (req, res) => {
     const secretKey = 'mysecretkey_goalstreet';
     try {
         console.log("url params are ", req.query.userId)
-        if(!req.query.userId){
+        if (!req.query.userId) {
             res.status(400).send('Missing required fields');
             return;
         }
 
         try {
-            const existingData=await HrForm.findById(req.query.userId);
-            if(existingData){
-                const payload = { role:'HR',hrUserId:req.query.userId };
+            const existingData = await HrForm.findById(req.query.userId);
+            if (existingData) {
+                const payload = { role: 'HR', hrUserId: req.query.userId };
                 const token = jwt.sign(payload, secretKey);
                 let transporter = nodemailer.createTransport({
                     service: 'gmail',
@@ -26,25 +26,35 @@ exports.sendAccessEmail = async (req, res) => {
                 let mailOptions = {
                     from: '123003253@sastra.ac.in',
                     to: 'cherry.workspace.mail@gmail.com',
-                    subject: 'Test Email',
-                    text: `This is a test email sent from Node.js using Nodemailer.
-                    You can access page at https://goalstreetbackend.vercel.app/workdata/add?authtoken=${token}&role=HR`
+                    subject: 'Welcome to Our HR Dashboard',
+                    text: `Hello,
+        
+        This is an automated message to provide you with the access link to our HR Dashboard.
+        
+        You can access the page at: https://dashboard.goalstreet.in/workdata/add?authtoken=${token}&role=HR
+        
+        Best regards,
+        Your HR Team`,
+                    html: `<p>Hello,</p>
+                           <p>This is an automated message to provide you with the access link to our HR Dashboard.</p>
+                           <p><a href="https://dashboard.goalstreet.in/workdata/add?authtoken=${token}&role=HR">Click here</a> to access the HR Dashboard.</p>
+                           <p>Best regards,<br>Your HR Team</p>`
                 };
                 let info = await transporter.sendMail(mailOptions);
                 console.log('Email sent: ' + info.response);
-        
+
                 // Respond to the client
                 console.log("sucessfully sent")
                 res.status(200).send('Email sent');
-                
+
             }
-        }catch(err){
+        } catch (err) {
             res.status(500).send('Error sending email');
         }
-        
 
 
-        
+
+
 
 
     } catch (error) {
